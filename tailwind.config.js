778465029f13b8e38ae7d82610b0809b6ec32c8d
xrekan <xrekandev@gmail.com>
2023-10-12 11:12:27 +0700

    lading page

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+  
+      
+      container: {
+        center: true,
+        padding: "16px",
+    },
+    extend: {
+        colors: {
+            "primary": "#14E314",
+        },
+    },
+    fontFamily: {
+        sans: ["Figtree", "sans-serif"],
+    },
+  },
+  plugins: [],
+}
+
